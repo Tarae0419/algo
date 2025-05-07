281d247611670009d6be7b5f6f1575ec2587728d
Tarae0419 <tarae0419@gmail.com>
2025-05-07 09:30:06 +0900

    [Silver II] Title: 최대 힙, Time: 1428 ms, Memory: 117336 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        for(int i = 0; i < N; i++) {
+            int X = sc.nextInt();
+
+            if(X == 0){
+                if(pq.isEmpty()){
+                    System.out.println(0);
+                }else{
+                    System.out.println(pq.poll());
+                }
+            }else{
+                pq.add(X);
+            }
+        }
+    }
+
+}
