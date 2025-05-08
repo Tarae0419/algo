7b77ab2301b5bf9eed7ae377e414e7c2a648cf2d
Tarae0419 <tarae0419@gmail.com>
2025-05-08 16:38:45 +0900

    [Silver III] Title: N과 M (2), Time: 192 ms, Memory: 18304 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static int[] arr;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+        M = sc.nextInt();
+        arr = new int[M];
+
+        dfs(0,1);
+    }
+    public static void dfs(int depth, int start){
+        if(depth == M){
+            for(int i = 0; i < M; i++){
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for(int i = start; i <= N; i++){
+            arr[depth] = i;
+            dfs(depth + 1, i + 1);
+        }
+    }
+}
