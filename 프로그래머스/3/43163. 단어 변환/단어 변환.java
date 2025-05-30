e18d16d08325fdc4ffc9e2586e7c3b8d99121b48
Tarae0419 <tarae0419@gmail.com>
2025-05-30 09:44:35 +0900

    [level 3] Title: 단어 변환, Time: 0.20 ms, Memory: 87.1 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -2,37 +2,34 @@ import java.util.*;
 class Solution {
     public int solution(String begin, String target, String[] words) {
         Set<String> checked = new HashSet<>();
-        Queue<String> queue  = new ArrayDeque<>();
-        queue.offer(begin);
-        checked.add(begin);
+        Queue<String> queue = new ArrayDeque<>();
         
-       int count = 0;  
-            
-        while(!queue.isEmpty()){
-            int size = queue.size();
-            for(int i = 0; i < size; i++){
-                String curStr = queue.poll();
-            
-                if(curStr.equals(target)) return count;
+        queue.offer(begin);
+        
+        int count = 0;
+        
+        while(!queue.isEmpty()){    
+            for(int i = 0; i < queue.size(); i++){
+                String curNode = queue.poll();
+                if(curNode.equals(target)) return count;
             
                 for(String word : words){
-                    if(checkDiff(curStr, word) && !checked.contains(word)){
-                        checked.add(word);
-                        queue.add(word);
+                    if(check(word, curNode) && !checked.contains(word)){
+                    queue.add(word);
+                    checked.add(word);
                     }
                 }
             }
             count++;
-        }
-        return 0;
+        }    
+       return 0; 
     }
-    
-    public boolean checkDiff(String str1, String str2){
-        int diff = 0;
-        for(int i = 0; i < str1.length(); i++){
-            if(str1.charAt(i) != str2.charAt(i)) diff++;
-            if(diff > 1) return false;
+    public static boolean check(String str1, String str2){
+            int diff = 0;
+            for(int i = 0; i < str1.length();i++){
+                if(str1.charAt(i) != str2.charAt(i)) diff++;
+                if(diff > 1) return false;
+            }
+            return true;
         }
-        return true;
-    }
 }
