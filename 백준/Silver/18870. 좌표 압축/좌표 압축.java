5380393da0f989d3e7e1dd158ac2471cee434a54
Tarae0419 <tarae0419@gmail.com>
2025-06-10 10:23:38 +0900

    [Silver II] Title: 좌표 압축, Time: 2016 ms, Memory: 279732 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -1,17 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
         Set<Integer> set = new HashSet<>();
         Map<Integer, Integer> map = new HashMap<>();
-        int N = sc.nextInt();
+        int N = Integer.parseInt(br.readLine());
         int[] arr = new int[N];
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 0; i < N; i++){
-            int a = sc.nextInt();
+            int a = Integer.parseInt(st.nextToken());
             set.add(a);
             arr[i] = a;
         }
