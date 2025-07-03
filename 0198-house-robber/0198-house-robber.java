d1a7481e2f52ae5b853fefeabd8eec942da9e830
Tarae0419 <tarae0419@gmail.com>
2025-07-03 12:52:19 +0900

    Time: 0 ms (100%), Space: 41.1 MB (46.85%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,9 +1,8 @@
 class Solution {
     public int rob(int[] nums) {
-        int[] dp = new int[45];
+        int[] dp = new int[101];
         
         if(nums.length == 1) return nums[0];
-        if(nums.length == 2) return Math.max(nums[0], nums[1]);
 
         dp[0] = nums[0];
         dp[1] = Math.max(nums[0], nums[1]);
