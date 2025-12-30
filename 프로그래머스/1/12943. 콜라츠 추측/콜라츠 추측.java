a2bb70783da87bd151efe6e460d1f0e64223a341
박준영 <tarae0419@gmail.com>
2025-12-30 17:00:11 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 98.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int num) {
+        if(num == 1) return 0;
+        for(int i = 0; i < 500; i++){
+            if(num == 1) return i;
+            else if(num % 2 == 0) num = num / 2;
+            else if(num % 2 == 1) num = num * 3 + 1;
+        }
+        return -1;
+    }
+}
