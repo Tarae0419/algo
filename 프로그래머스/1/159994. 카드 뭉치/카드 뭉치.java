3a318286647f00fea9292ab8a0cfd252d6af96cd
박준영 <tarae0419@gmail.com>
2026-01-18 23:26:45 +0900

    [level 1] Title: 카드 뭉치, Time: 0.03 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        
+        int card1 = 0;
+        int card2 = 0;
+        
+        for(String word : goal){
+            if(card1 < cards1.length && cards1[card1].equals(word)) card1++;
+            else if(card2 < cards2.length && cards2[card2].equals(word)) card2++;
+            else return "No";
+        }
+        
+        return "Yes";
+    }
+}
