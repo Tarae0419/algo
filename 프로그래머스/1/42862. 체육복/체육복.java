4736f2b0d54a9e4b64442c19d67fc595ac9839b7
박준영 <tarae0419@gmail.com>
2026-03-22 23:29:33 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int[] students = new int[n + 1];
+        for(int i = 1; i <= n; i++) students[i] = 1;
+        
+        for(int l : lost) students[l]--;
+        for(int r : reserve) students[r]++;
+        
+        for(int i = 1; i <= n; i++) {
+            if(students[i] == 0)
+                if(i - 1 >= 1 && students[i - 1] == 2) {
+                    students[i - 1]--;
+                    students[i]++;
+                } 
+                else if(i + 1 <= n && students[i + 1] == 2) {
+                    students[i + 1]--;
+                    students[i]++;
+                }
+            }
+        
+        int answer = 0;
+        for(int i = 1; i <= n; i++){
+            if(students[i] >= 1) answer++;
+        }
+        return answer;
+    }
+}
