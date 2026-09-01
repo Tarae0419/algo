8d9b8151d1b1be3972cf73f1ddbcb3f018475546
박준영 <tarae0419@gmail.com>
2026-09-01 18:50:33 +0900

    Time: 35 ms (42.41%), Space: 59 MB (86.15%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -6,8 +6,8 @@ class Solution {
 
         Arrays.sort(nums);
 
-        for(int i = 0; i < nums.length - 2; i++){
-            if(i > 0 && nums[i] == nums[i - 1]) continue;
+        for(int i = 0; i < nums.length - 2; i ++){
+            if( i > 0 && nums[i] == nums[i - 1]) continue;
 
             int left = i + 1;
             int right = nums.length - 1;
@@ -16,18 +16,17 @@ class Solution {
                 int sum = nums[i] + nums[left] + nums[right];
 
                 if(sum == 0){
-                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    result.add(Arrays.asList(nums[left], nums[i], nums[right]));
 
-                    while (left < right && nums[left] == nums[left + 1]) left++;
-                    while (left < right && nums[right] == nums[right - 1]) right--;
-                    
-                    left++;
-                    right--;
-                }else if(sum > 0) right--;
-                else left++;
+                    left++; right--;
+
+                    while(left < right && nums[left] == nums[left - 1]) left++;
+                    while(left < right && nums[right] == nums[right + 1]) right--;
+                }
+                else if(sum < 0) left++;
+                else right--;
             }
         }
-
         return result;
     }
 }
